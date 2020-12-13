c778fc201f06b6e21b86ae359d189ef7b88adb59
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2020-12-13 20:11:06 +0100

    Final ejemplo

---
+++ Assets/Prueba.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    int frames = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frames++;
+
+        if (frames %100 == 0)
+        {
+            Debug.Log("100 frames mas");
+        }
+    }
+}
